eddc32253ea35b81015d165dd574ceee8d4614ee
Alexandros Matenoglou <alexandros.matenoglou@gmx.de>
2020-09-04 13:52:18 +0200

    walkthrough - step 16

--- dist/controller/App-dbg.controller.js
+++ dist/controller/App-dbg.controller.js
@@ -4,12 +4,6 @@ sap.ui.define([
 ],
 function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-        onShowHello: function () {
-            var oBundle = this.getView().getModel("i18n").getResourceBundle();
-            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-            MessageToast.show(sMsg);
-        }
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App",{
     });
 });
