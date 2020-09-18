25ed11fc9ad0a104ff1706be84ba9ce5b3e1ecdd
Alexandros Matenoglou <alexandros.matenoglou@gmx.de>
2020-09-18 16:18:45 +0200

    walkthrough - step 36

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,34 +1,43 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/model/json/JSONModel",
-	"./controller/HelloDialog"
-
-], function (UIComponent, JSONModel, HelloDialog) {
+	"./controller/HelloDialog",
+	"sap/ui/Device"
+], function (UIComponent, JSONModel, HelloDialog, Device) {
 	"use strict";
+
 	return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-		metadata : {
-			manifest : "json"
+
+		metadata: {
+			manifest: "json"
 		},
-		init : function () {
+
+		init: function () {
 			// call the init function of the parent
 			UIComponent.prototype.init.apply(this, arguments);
+
 			// set data model
 			var oData = {
-				recipient : {
-					name : "World"
+				recipient: {
+					name: "World"
 				}
 			};
 			var oModel = new JSONModel(oData);
 			this.setModel(oModel);
 
+			// set device model
+			var oDeviceModel = new JSONModel(Device);
+			oDeviceModel.setDefaultBindingMode("OneWay");
+			this.setModel(oDeviceModel, "device");
+
 			// set dialog
 			this._helloDialog = new HelloDialog(this.getRootControl());
+
 			// create the views based on the url/hash
 			this.getRouter().initialize();
 		},
 
-
-		exit : function() {
+		exit : function () {
 			this._helloDialog.destroy();
 			delete this._helloDialog;
 		},
@@ -36,5 +45,7 @@ sap.ui.define([
 		openHelloDialog : function () {
 			this._helloDialog.open();
 		}
+
 	});
+
 });
